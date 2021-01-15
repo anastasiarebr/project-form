9bdffe009e905f3af3030907f94a38a9735f444f
Anastasia <anastasiarebr@gmail.com>
2021-01-15 15:13:49 +0300

    login

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const routes: Array<RouteRecordRaw> = [
     component: Login
   },
   {
+    path: '/loginpassword',
+    name: 'Password',
+    component: () => import('@/views/LoginPassword.vue')
+  },
+  {
     path: '/register',
     name: 'Register',
     component: () => import('@/views/Register.vue')
--- src/views/LoginPassword.vue
+++ src/views/LoginPassword.vue
@@ -1,22 +1,30 @@
 <template>
   <div class="container">
     <div class="column">
-      <form class="form">
+      <form class="form" @submit.prevent="onSubmit">
         <div class="form__title">Вход в личный кабинет</div>
         <div class="form__input">
           <input
-            type="text"
-            id="login"
+            type="password"
+            id="password"
             class="input"
-            v-model="login"
-            placeholder="e.g. john@gmail.com"
+            v-model="password"
+            placeholder="8 символов"
+            @blur="checkPassword"
+            :class="{ input_error: !validPassword }"
           />
-          <label for="login" class="label"
-            >Номер телефона или email</label
+          <label
+            for="password"
+            class="label"
+            :class="{ label_error: !validPassword }"
+            >Пароль</label
           >
         </div>
-        <router-link to="/loginpassword" class="form__btn">
-          <div class="form__btn_text">Войти</div>
+        <div class="form__link">
+          <router-link to="/">Забыли пароль?</router-link>
+        </div>
+        <button class="form__btn ripple" type="submit">
+          Войти
           <div class="icon">
             <svg
               width="16"
@@ -31,25 +39,8 @@
               />
             </svg>
           </div>
-        </router-link>
+        </button>
       </form>
-      <router-link to="/register" class="form__btn form__btn_reg">
-        Регистрация
-        <div class="icon">
-          <svg
-            width="16"
-            height="16"
-            viewBox="0 0 16 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
-              fill="#56CCF2"
-            />
-          </svg>
-        </div>
-      </router-link>
     </div>
   </div>
 </template>
