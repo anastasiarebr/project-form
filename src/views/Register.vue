023fed026f28ddbf42cd7f1cdda1baae9281a9cf
Anastasia <anastasiarebr@gmail.com>
2021-01-27 16:29:19 +0300

    timer for code

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -3,169 +3,211 @@
     <div class="column">
       <form action="" class="form" @submit.prevent="onRegister()">
         <transition name="slide-fade">
-          <section v-if="step===1">
-          <div class="form__title">Регистрация нового личного кабинета</div>
-          <div class="form__input">
-            <input
-              type="text"
-              id="phone"
-              class="input"
-              v-model="phone"
-              @blur="checkPhone"
-              :class='{input_error: !validPhone}'
-            />
-            <label for="phone" class="label" :class='{label_error: !validPhone}'>Номер телефона</label>
-          </div>
-          <button type="button" :disabled="phone.length===0" @click.prevent="nextStep()" class="form__btn form__btn_sign">
-            <div class="form__btn_text">
-            Получить код
-            </div>
-            <div class="icon">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+          <section v-if="step === 1">
+            <div class="form__title">Регистрация нового личного кабинета</div>
+            <div class="form__input">
+              <input
+                type="text"
+                id="phone"
+                class="input"
+                v-model="phone"
+                @blur="checkPhone"
+                :class="{ input_error: !validPhone }"
+              />
+              <label
+                for="phone"
+                class="label"
+                :class="{ label_error: !validPhone }"
+                >Номер телефона</label
               >
-                <path
-                  d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
-                  fill="white"
-                />
-              </svg>
             </div>
-          </button>
+            <button
+              type="button"
+              :disabled="phone.length === 0"
+              @click.prevent="phoneStep()"
+              class="form__btn form__btn_sign"
+            >
+              <div class="form__btn_text">Получить код</div>
+              <div class="icon">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </button>
           </section>
         </transition>
 
         <transition name="slide-fade">
-          <section v-if="step===2">
+          <section v-if="step === 2">
             <div class="form__title">Подтверждение регистрации</div>
-          <div class="form__phone">
-            <div class="form__phone_label">Номер телефона</div>
-            <div class="form__phone_number">{{ phone }}</div>
-            <a @click.prevent="step--">
-            <div class="form__phone_changenumber">
-            Указать другой номер
-            </div></a>
-          </div>
-          <div class="form__input">
-            <input
-              type="text"
-              id="code"
-              class="input"
-              v-model="code"
-              placeholder="0000"
-              @blur="checkCode"
-              :class='{input_error: !validCode}'
-            />
-            <label for="code" class="label" :class='{label_error: !validCode}'>Код подтверждения</label>
-          </div>
-          <button type="button" :disabled="code.length!=4" @click.prevent="codeStep()" class="form__btn form__btn_sign">
-            <div class="form__btn_text">
-            Отправить код
-            </div>
-            <div class="icon">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <div class="form__phone">
+              <div class="form__phone_label">Номер телефона</div>
+              <div class="form__phone_number">{{ phone }}</div>
+              <a @click.prevent="changeNumber">
+                <div class="form__phone_changenumber">
+                  Указать другой номер
+                </div></a
               >
-                <path
-                  d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
-                  fill="white"
-                />
-              </svg>
             </div>
-          </button>
-          <div class="form__text">Не получили код?</div>
-          <div class="form__sendcode"><a href="">Выслать повторно</a></div>
+            <div class="form__input">
+              <input
+                type="text"
+                id="code"
+                class="input"
+                v-model="code"
+                placeholder="0000"
+                @blur="checkCode"
+                :class="{ input_error: !validCode }"
+              />
+              <label
+                for="code"
+                class="label"
+                :class="{ label_error: !validCode }"
+                >Код подтверждения</label
+              >
+            </div>
+            <button
+              type="button"
+              :disabled="code.length != 4"
+              @click.prevent="codeStep()"
+              class="form__btn form__btn_sign"
+            >
+              <div class="form__btn_text">Отправить код</div>
+              <div class="icon">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
+                    fill="white"
+                  />9
+                </svg>
+              </div>
+            </button>
+            <div class="form__text">Не получили код?</div>
+            <div class="form__sendcode form__sendcode_notactive" v-if="(timerCount > 0) && (!skip) ">Выслать повторно через {{ timerCount }}</div>
+            <div class="form__sendcode" v-if="(timerCount === 0) && (code==='' || !validCode) && (!skip)" @click="errorCode"><a>Выслать повторно</a></div>
+            <div class="form__sendcode form__sendcode_notactive" v-if="(timerCount > 0) && skip">Пропустить через {{ timerCount }}</div>
+            <div class="form__sendcode" v-if="(timerCount === 0) && skip" @click="step++"><a>Пропустить</a></div>
           </section>
         </transition>
 
         <transition name="slide-fade">
-          <section v-if="step===3">
+          <section v-if="step === 3">
             <div class="form__title">Подтвердите электронную почту</div>
-          <div class="form__input">
-            <input
-              type="email"
-              id="email"
-              placeholder="e.g. john@gmail.com"
-              class="input"
-              v-model.trim="email"
-              @blur="checkEmail"
-              :class='{input_error: !validEmail}'
-            />
-            <label for="email" class="label" :class='{label_error: !validEmail}'>Email</label>
-          </div>
-          <button type="button" :disabled="email.length===0" @click="emailStep()" class="form__btn form__btn_sign">
-            <div class="form__btn_text">
-            Продолжить
-            </div>
-            <div class="icon">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <div class="form__input">
+              <input
+                type="email"
+                id="email"
+                placeholder="e.g. john@gmail.com"
+                class="input"
+                v-model.trim="email"
+                @blur="checkEmail"
+                :class="{ input_error: !validEmail }"
+              />
+              <label
+                for="email"
+                class="label"
+                :class="{ label_error: !validEmail }"
+                >Email</label
               >
-                <path
-                  d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
-                  fill="white"
-                />
-              </svg>
             </div>
-          </button>
+            <button
+              type="button"
+              :disabled="email.length === 0"
+              @click="emailStep()"
+              class="form__btn form__btn_sign"
+            >
+              <div class="form__btn_text">Продолжить</div>
+              <div class="icon">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </button>
           </section>
         </transition>
 
         <transition name="slide-fade">
-          <section v-if="step===4">
+          <section v-if="step === 4">
             <div class="form__title">Придумайте пароль</div>
-          <div class="form__input">
-            <input
-              type="password"
-              id="password"
-              placeholder="8 символов"
-              class="input"
-              v-model="password"
-              @blur="checkPassword"
-              :class='{input_error: !validPassword}'
-            />
-            <label for="password" class="label" :class='{label_error: !validPassword}'>Пароль</label>
-          </div>
-          <div class="form__input">
-            <input
-              type="password"
-              id="confirmPassword"
-              placeholder="8 символов"
-              class="input"
-              v-model="confirmPassword"
-              @blur="checkConfirmPasswords"
-              :class='{input_error: !validConfirmPassword}'
-            />
-            <label for="confirmPassword" class="label" :class='{label_error: !validConfirmPassword}'>Пароль повторно</label>
-          </div>
-          <button @click.prevent="onRegister()" type="submit" class="form__btn form__btn_sign">
-            Завершить регистрацию
-            <div class="icon">
-              <svg
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+            <div class="form__input">
+              <input
+                type="password"
+                id="password"
+                placeholder="8 символов"
+                class="input"
+                v-model="password"
+                @blur="checkPassword"
+                :class="{ input_error: !validPassword }"
+              />
+              <label
+                for="password"
+                class="label"
+                :class="{ label_error: !validPassword }"
+                >Пароль</label
               >
-                <path
-                  d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
-                  fill="white"
-                />
-              </svg>
             </div>
-          </button>
+            <div class="form__input">
+              <input
+                type="password"
+                id="confirmPassword"
+                placeholder="8 символов"
+                class="input"
+                v-model="confirmPassword"
+                @blur="checkConfirmPasswords"
+                :class="{ input_error: !validConfirmPassword }"
+              />
+              <label
+                for="confirmPassword"
+                class="label"
+                :class="{ label_error: !validConfirmPassword }"
+                >Пароль повторно</label
+              >
+            </div>
+            <button
+              @click.prevent="onRegister()"
+              type="submit"
+              class="form__btn form__btn_sign"
+            >
+              Завершить регистрацию
+              <div class="icon">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M0 7H12.17L6.58 1.41L8 0L16 8L8 16L6.59 14.59L12.17 9H0V7Z"
+                    fill="white"
+                  />
+                </svg>
+              </div>
+            </button>
           </section>
         </transition>
       </form>
@@ -175,6 +217,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+
 export default defineComponent({
   data () {
     return {
@@ -189,12 +232,16 @@ export default defineComponent({
       validPassword: true,
       validConfirmPassword: true,
       validPhone: true,
-      validCode: true
+      validCode: true,
+      timerCount: 10,
+      skip: false
     }
   },
   methods: {
-    nextStep () {
+    phoneStep () {
+      this.timerCount = 10
       this.step++
+      this.countDownTimer()
     },
     checkEmail () {
       const regExp = new RegExp(/.+@.+\..+/i)
@@ -209,10 +256,16 @@ export default defineComponent({
       this.validPassword = !(this.password.length < this.minLength)
     },
     checkConfirmPasswords () {
-      this.validConfirmPassword = Boolean(this.password === this.confirmPassword)
+      this.validConfirmPassword = Boolean(
+        this.password === this.confirmPassword
+      )
     },
     checkPhone () {
       this.validPhone = Boolean(this.phone.length)
+    },
+    changeNumber () {
+      this.step--
+      this.skip = !this.skip
     },
     checkCode () {
       this.validCode = Boolean(this.code === '1111')
@@ -243,6 +296,20 @@ export default defineComponent({
       } else {
         console.log('errors')
       }
+    },
+    countDownTimer () {
+      if (this.timerCount > 0) {
+        setTimeout(() => {
+          this.timerCount--
+          this.countDownTimer()
+        }, 1000)
+      }
+    },
+    errorCode () {
+      console.log(this)
+      this.skip = true
+      this.timerCount = 10
+      this.countDownTimer()
     }
   }
 })
